5b0325654b917da0e9df6151f40a2b845c751760
Harshit Ajakiya <96171251+harshit3134@users.noreply.github.com>
2022-12-27 21:15:34 +0530

    Array

---
+++ 485_MaxConsecutiveOnes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==1)
+            count++;
+            if(count>=ans)
+            ans=count;
+            if(nums[i]==0)
+            count=0;
+        }
+        return ans;
+    }
+};
