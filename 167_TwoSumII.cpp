cb31b2d4305f19e860bce3b0d05ec80dd2b50789
Harshit Ajakiya <96171251+harshit3134@users.noreply.github.com>
2022-12-27 21:14:06 +0530

    Array

---
+++ 167_TwoSumII.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int>ans;
+        int l=0;
+        int h=numbers.size()-1;
+        while(l<=h){
+            if(numbers[l]+numbers[h]==target)
+            break;
+            if(numbers[l]+numbers[h]<target)
+            l++;
+            else
+            h--;
+        }
+        ans.push_back(l+1);
+        ans.push_back(h+1);
+        return ans;
+    }
+};
